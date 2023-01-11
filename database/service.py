1b787b306147135d822a16ac7c0dfc0df82775ba
Luke Moorhous <36687832+lukemoorhous@users.noreply.github.com>
2023-01-11 13:31:44 -0600

    working out a new insert method

--- database/service.py
+++ database/service.py
@@ -1,11 +1,16 @@
 import sqlite3
-from data import ContentFile, ContentVersion
+from data import DTO,ContentFile, ContentVersion
 
 database_location = 'taxonomical-database.db'
 
-CREATE_STRINGS = {
-    'ContentFile' : "CREATE TABLE ContentFile(filename VARCHAR(255), filetype VARCHAR(255), fileextension: VARCHAR(25))",
-    'ContentVersion': "CREATE TABLE ContentVersion(FOREIGN KEY(content_file) REFERENCES ContentFile(id), name VARCHAR(255), version VARCHAR(255), contents BLOB)"
+CREATE_STRING = {
+    'ContentFile' : "CREATE TABLE ContentFile(id INTEGER primary key autoincrement, filename VARCHAR(255), filetype VARCHAR(255), fileextension: VARCHAR(25))",
+    'ContentVersion': "CREATE TABLE ContentVersion(id INTEGER primary key autoincrement, FOREIGN KEY(content_file) REFERENCES ContentFile(id), name VARCHAR(255), version VARCHAR(255), contents BLOB)"
+}
+
+COLUMN_LIST = {
+    'ContentFile' : ['id', 'filename', 'filetype', 'fileextension'],
+    'ContentVersion' : ['id', 'name', 'version', 'content_file', 'contents']
 }
 
 class Database_impl:
@@ -15,6 +20,13 @@ class Database_impl:
 
     def connection(self):
         return sqlite3.connect(self.location)
+
+    def create(self, query):
+        con = self.connection()
+        cur = con.cursor()
+        cur.execute(query)
+        con.commit()
+        con.close()
 
     def __database_tables__(self, cur):
         database_tables = []
@@ -29,19 +41,40 @@ class Database_impl:
         database_tables = self.__database_tables__(cur)
         for table in self.tables:
             if not database_tables.__contains__(table.__name__):
-                cur.execute(CREATE_STRINGS.get(table.__name__))
+                cur.execute(CREATE_STRING.get(table.__name__))
         con.commit()
         con.close()
 
-    def insert(self, content_files: list):
-        con = self.connection()
+    def insert_ContentFiles(self, records: list[ContentFile]):
         values = ''
-        for record in content_files:
-            values += f"{record.__value__()},"
+        
+        for record in records:
+            values += f"{record.__repr__()},"
         values = values.removesuffix(',')
-        query = f"INSERT INTO {ContentFile.__name__} VALUES {values}"
-        con.cursor().execute(query)
-        con.commit()
-        con.close()
+        query = f"INSERT INTO {ContentFile.__name__} ({COLUMN_LIST.get(ContentFile.__name__).__str__().strip('[,]')}) VALUES {values}"
+        # print(query)
+        self.create(query)
+
+    def insert(self, records: list[DTO]):
+        if records.__sizeof__() <= 0:
+            return None
+        cls = records[0].__class__
+        cols = COLUMN_LIST.get(cls.__name__)
+        records_vals = None
+        for record in records:
+            if record.__class__ != cls:
+                raise TypeError('Cannot Insert multiple types in one operation')
+            record_vals = None
+            for col in cols:
+                val = record.__getattribute__(col)
+                if record_vals is None:
+                    record_vals = f"'{val}'"
+                else:
+                    record_vals = f"{record_vals}, '{val}'"
+            if records_vals is None:
+                records_vals = f"({record_vals})"
+            else:
+                records_vals = f"{records_vals}, ({record_vals})"
+        print(records_vals)
 
 DATABASE = Database_impl()
--- database/tests/service_test.py
+++ database/tests/service_test.py
@@ -7,15 +7,30 @@ class DATABASE_test(unittest.TestCase):
 
     def test_init(self):
         self.assertNotEqual(None, DATABASE.location)
-    
+
     def test_tablesCreated(self):
         tables = DATABASE.__database_tables__(DATABASE.connection().cursor())
-        
+
         self.assertTrue(tables.__contains__(ContentFile.__name__))
         self.assertTrue(tables.__contains__(ContentVersion.__name__))
+
+    # def test_insertContentFile(self):
+        # cFiles = [ContentFile(filename='kek')]
+        # result = DATABASE.insert_ContentFiles(records=cFiles)
+
+        # print(result)
     
-    def test_insertContentFile(self):
+    def test_insert(self):
         cFiles = [ContentFile(filename='kek')]
+        DATABASE.insert(records=cFiles)
+    
+    def test_insert_multipleTypes(self):
+        records = [ContentFile(filename='kek'), ContentVersion(name='notAFile')]
+
+        caught = False;
+        try:
+            DATABASE.insert(records)
+        except TypeError:
+            caught = True
         
-        result = DATABASE.insert(cFiles)
-        print(result)
+        self.assertTrue(caught)
